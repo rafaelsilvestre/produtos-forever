6cc06cf3a2e7d3b77e07e8bb8e9e8ba67ddfc38a
Rafael Silvestre <rafaelsilvestre.183@gmail.com>
2019-03-27 08:27:20 -0300

    Clappr video

--- src/pages/want-to-be-partner/want-to-be-partner.ts
+++ src/pages/want-to-be-partner/want-to-be-partner.ts
@@ -1,14 +1,37 @@
-import { Component, Input } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
     selector: 'page-want-to-be-partner',
     templateUrl: './want-to-be-partner.html',
     styleUrls: ['./want-to-be-partner.scss']
 })
-export class WantToBePartnerPageComponent {
+export class WantToBePartnerPageComponent implements AfterViewInit {
+    player: any;
     path: Array<any> = [
         {path: '/', title: 'Home'},
         {path: '/quero-ser-parceiro', title: 'Quero ser parceiro', active: true}
     ];
+
+    ngAfterViewInit(): void {
+        const playerElement = (<any>window).document.getElementById('player-wrapper');
+        this.player = new (<any>window).Clappr.Player({
+            source: 'https://s3-sa-east-1.amazonaws.com/produtos-forever/want-to-be-partner.mp4',
+            mute: false,
+            height: '100%',
+            width: '100%'
+        });
+        this.player.attachTo(playerElement);
+
+        this.resizePlayer();
+        (<any>window).onresize = this.resizePlayer;
+    }
+
+    resizePlayer() {
+        const aspectRatio = 9 / 16;
+        const newWidth = (<any>window).document.getElementById('player-wrapper').parentElement.offsetWidth;
+        const newHeight = 2 * Math.round(newWidth * aspectRatio / 2);
+
+        this.player.resize({width: newWidth, height: newHeight});
+    }
 }
 
